0254d6c363e53f76fed611624cfa72dc9965e194
Frank Martinez <fmartinez@tibco.com>
2018-11-02 00:50:09 -0400

    add embed and shim support

---
+++ util/env.go
@@ -0,0 +1,15 @@
+package util
+
+import "strings"
+
+func ReplaceEnvValue(env []string, envKey string, newValue string) []string {
+	for key, entry := range env {
+
+		if strings.HasPrefix(entry, envKey + "=") {
+			env[key] = envKey + "=" + newValue
+			break
+		}
+	}
+
+	return env
+}
