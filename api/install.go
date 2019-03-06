8b6ba66109994822bb30197059cb97b2ac4c0c50
skothari-tibco <skothari@tibco.com>
2019-03-05 21:54:44 -0500

    Edit

--- api/install.go
+++ api/install.go
@@ -66,7 +66,7 @@ func InstallContribBundle(project common.AppProject, path string) error {
 	}
 
 	for _, contrib := range contribBundleDescriptor.Contribs {
-		InstallPackage(project, fmt.Sprintf("%v", contrib))
+		InstallPackage(project, contrib)
 	}
 
 	return nil
