539344683c47bb043dba3e455eb3fcaca03ddc47
skothari-tibco <skothari@tibco.com>
2019-03-19 20:51:12 -0400

    Add sync and resolve

--- api/sync.go
+++ api/sync.go
@@ -1,22 +1,29 @@
 package api
 
 import (
-	"os/exec"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/project-flogo/cli/common"
 	"github.com/project-flogo/cli/util"
+	"github.com/project-flogo/core/app"
 )
 
 func SyncPkg(project common.AppProject) error {
 
-	pkgs, err := util.GetImportsFromJSON(filepath.Join(project.Dir(), "flogo.json"))
+	pkgs, err := util.GetAppImports(filepath.Join(project.Dir(), fileFlogoJson), project.DepManager(), true)
 
 	if err != nil {
 		return err
 	}
+	for _, pkg := range pkgs.GetAllImports() {
+		project.RemoveImports(pkg.GoImportPath())
+	}
 
-	err = updateGoMod(project, pkgs)
+	err = updateGoMod(project, pkgs.GetAllImports())
 
 	if err != nil {
 		return err
@@ -26,13 +33,11 @@ func SyncPkg(project common.AppProject) error {
 }
 func updateGoMod(project common.AppProject, pkgs util.Imports) error {
 
-	err := clearGoMod(project.SrcDir())
-
-	if err != nil {
-		return err
-	}
-
+	var err error
 	for _, pkg := range pkgs {
+		if Verbose() {
+			fmt.Println("Adding dependency for ", pkg)
+		}
 		err = project.DepManager().AddDependency(pkg)
 		if err != nil {
 			return err
@@ -41,19 +46,64 @@ func updateGoMod(project common.AppProject, pkgs util.Imports) error {
 	return nil
 }
 
-func clearGoMod(src string) error {
-	err := util.ExecCmd(exec.Command("rm", "go.mod"), src)
+func ResolvePkg(project common.AppProject) error {
+	/*err := SyncPkg(project)
+	if err != nil {
+		return err
+	}*/
+	imports, err := project.DepManager().GetAllImports()
 	if err != nil {
 		return err
 	}
-	err = util.ExecCmd(exec.Command("rm", "go.sum"), src)
-	if err != nil {
-		return err
-	}
-	err = util.ExecCmd(exec.Command("go", "mod", "init", "main"), src)
-	if err != nil {
-		return err
-	}
-	return nil
 
+	addImportToJSON(project, imports)
+
+	return nil
+}
+
+func addImportToJSON(project common.AppProject, imports map[string]util.Import) error {
+	appDescriptorFile := filepath.Join(project.Dir(), fileFlogoJson)
+	appDescriptorJsonFile, err := os.Open(appDescriptorFile)
+	if err != nil {
+		return err
+	}
+	defer appDescriptorJsonFile.Close()
+
+	appDescriptorData, err := ioutil.ReadAll(appDescriptorJsonFile)
+	if err != nil {
+		return err
+	}
+
+	var appDescriptor app.Config
+	json.Unmarshal([]byte(appDescriptorData), &appDescriptor)
+
+	parsedImports, err := util.ParseImports(appDescriptor.Imports)
+
+	if err != nil {
+		return err
+	}
+
+	var result []string
+
+	for _, Import := range parsedImports {
+
+		if val, ok := imports[Import.ModulePath()]; ok {
+			result = append(result, val.CanonicalImport())
+		}
+
+	}
+
+	appDescriptor.Imports = result
+
+	appDescriptorUpdated, err := json.MarshalIndent(appDescriptor, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(appDescriptorFile, []byte(appDescriptorUpdated), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
